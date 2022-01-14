4d4c367cdb457ce6cd0b072cde987a8f6a81d3d1
Carlos <lamark.barros@gmail.com>
2022-01-14 00:14:15 -0300

    quase nada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from index import *
 
 
 class Application:
@@ -7,57 +8,36 @@ class Application:
         self.widget1.pack()
 
         self.msg = Label(self.widget1, text="Armazenamento de Dados")
-        self.msg.pack ()
+        self.msg.pack()
 
         self.clients = Button(self.widget1)
         self.clients["text"] = "Clientes"
         self.clients["font"] = ("Calibri", "10")
         self.clients["width"] = 10
         self.clients["command"] = self.clientsButton
-        self.clients.pack ()
+        self.clients.pack()
 
         self.prod = Button(self.widget1)
         self.prod["text"] = "Produtos"
         self.prod["font"] = ("Calibri", "10")
         self.prod["width"] = 10
         # self.prod["command"] = self.widget1.quit
-        self.prod.pack ()
+        self.prod.pack()
 
     def clientsButton(self, master=None):
-        self.widget1.destroy()
-
         self.widget2 = Frame(master)
         self.widget2.pack()
 
+        self.msg = Label(self.widget1, text="Clientes")
+        self.msg.pack()
 
-        self.cad = Button(self.widget2)
-        self.cad["text"] = "Cadastrar Clientes"
-        self.cad["font"] = ("Calibri", "10")
-        self.cad.pack()
 
-        self.show = Button(self.widget2)
-        self.show["text"] = "Ver Clientes"
-        self.show["font"] = ("Calibri", "10")
-        self.show.pack()
 
-        self.back = Button(self.widget2)
-        self.back["text"] = "Voltar"
-        self.back["font"] = ("Calibri", "10")
-        self.back["command"] = self.widget2.destroy()
-        self.back.pack()
 
-    def prodButton(self):
-        self.clients.destroy()
-        self.prod.destroy()
 
-        self.cad = Button(self.widget1)
-        self.cad["text"] = "Cadastrar Produtos"
-        self.cad["font"] = ("Calibri", "10")
-        self.cad.pack()
-
-        self.show = Button(self.widget1)
-        self.show["text"] = "Ver Estoque"
-        self.show["font"] = ("Calibri", "10")
+    def prodButton(self, master=None):
+        self.widget2 = Frame(master)
+        self.widget2.pack()
 
 
 
